463fd15b092497d817538f9a12d50ca38b97df4a
Antonio Goncalves <antonio.goncalves@gmail.com>
2013-09-14 15:17:03 +0200

    Fixing stuff

---
+++ 01-Video/src/main/java/org/agoncal/sample/jsf/video/Talk.java
@@ -0,0 +1,99 @@
+package org.agoncal.sample.jsf.video;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Entity
+@Table(name = "JSFTalk")
+public class Talk implements Serializable {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Version
+    private int version;
+    @NotNull
+    private String title;
+    private String description;
+    @NotNull
+    private String room;
+    private String speaker;
+    private byte[] video;
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+
+    public Talk() {
+    }
+
+    public Talk(String title, String description, String room) {
+        this.title = title;
+        this.description = description;
+        this.room = room;
+    }
+
+    // ======================================
+    // =          Getters & Setters         =
+    // ======================================
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    public byte[] getVideo() {
+        return video;
+    }
+
+    public void setVideo(byte[] video) {
+        this.video = video;
+    }
+}
---
+++ 01-Video/src/main/java/org/agoncal/sample/jsf/video/TalkBean.java
@@ -0,0 +1,170 @@
+package org.agoncal.sample.jsf.video;
+
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateful;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+@Named
+@Stateful
+@ConversationScoped
+public class TalkBean implements Serializable {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+
+    private Long id;
+    private Talk talk;
+    private List<Talk> pageItems;
+    private Talk example = new Talk();
+
+    @Inject
+    private Conversation conversation;
+
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
+    // ======================================
+    // =          Business Methods          =
+    // ======================================
+
+    public String create() {
+
+        this.conversation.begin();
+        return "create?faces-redirect=true";
+    }
+
+    public void retrieve() {
+
+        if (FacesContext.getCurrentInstance().isPostback()) {
+            return;
+        }
+
+        if (this.conversation.isTransient()) {
+            this.conversation.begin();
+        }
+
+        if (this.id == null) {
+            this.talk = this.example;
+        } else {
+            this.talk = findById(getId());
+        }
+    }
+
+    public Talk findById(@NotNull Long id) {
+
+        return this.entityManager.find(Talk.class, id);
+    }
+
+    public String update() {
+        this.conversation.end();
+
+        try {
+            if (this.id == null) {
+                this.entityManager.persist(this.talk);
+                return "search?faces-redirect=true";
+            } else {
+                this.entityManager.merge(this.talk);
+                return "view?faces-redirect=true&id=" + this.talk.getId();
+            }
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+            return null;
+        }
+    }
+
+    public String delete() {
+        this.conversation.end();
+
+        try {
+            Talk deletableEntity = findById(getId());
+
+            this.entityManager.remove(deletableEntity);
+            this.entityManager.flush();
+            return "search?faces-redirect=true";
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getMessage()));
+            return null;
+        }
+    }
+
+    public void paginate() {
+
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Talk> criteria = builder.createQuery(Talk.class);
+        Root<Talk>root = criteria.from(Talk.class);
+        TypedQuery<Talk> query = this.entityManager.createQuery(criteria.select(root).where(getSearchPredicates(root)));
+        this.pageItems = query.getResultList();
+    }
+
+    private Predicate[] getSearchPredicates(Root<Talk> root) {
+
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        List<Predicate> predicatesList = new ArrayList<>();
+
+        String title = this.example.getTitle();
+        if (title != null && !"".equals(title)) {
+            predicatesList.add(builder.like(builder.lower(root.<String>get("title")), "%" + title.toLowerCase() + "%"));
+        }
+        String room = this.example.getRoom();
+        if (room != null && !"".equals(room)) {
+            predicatesList.add(builder.like(builder.lower(root.<String>get("room")), '%' + room.toLowerCase() + '%'));
+        }
+
+        return predicatesList.toArray(new Predicate[predicatesList.size()]);
+    }
+
+    // ======================================
+    // =          Getters & Setters         =
+    // ======================================
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Talk getTalk() {
+        return this.talk;
+    }
+
+    public Talk getExample() {
+        return this.example;
+    }
+
+    public void setExample(Talk example) {
+        this.example = example;
+    }
+
+    public List<Talk> getPageItems() {
+        return this.pageItems;
+    }
+}
